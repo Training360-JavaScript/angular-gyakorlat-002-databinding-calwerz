daa7aa7ccc47fc03c0f55e8146fe8e9416c57cb7
Daniel Biro <Daniel.Biro@evosoft.com>
2022-10-20 10:00:12 +0200

    24/24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { orderByPipe } from './order-by-pipe';
 import { PricingPageComponent } from './pricing-page/pricing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PricingPageComponent,
+    orderByPipe
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order-by-pipe.spec.ts
@@ -0,0 +1,7 @@
+import { orderByPipe } from './order-by-pipe';
+
+describe('OrderByPipe', () => {
+  it('should create an instance', () => {
+    expect(new orderByPipe()).toBeTruthy();
+  });
+});
